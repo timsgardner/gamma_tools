2fb774368763cb1c75975043a92fa71a98aef79d
timsgardner <timsgardner@gmail.com>
2015-11-10 17:26:55 -0500

    adding a pile of functionality and stuff

--- src/gamma_tools/core.clj
+++ src/gamma_tools/core.clj
@@ -1,9 +1,11 @@
 (ns gamma-tools.core
-  (require
-    [clojure.clr.io :as io]
-    [clojure.string :as string]
-    [gamma.api :as g]
-    [gamma.program :as p])
+  (:refer-clojure :exclude [aget])
+  (:require
+   [clojure.clr.io :as io]
+   [clojure.string :as string]
+   [gamma.api :as g]
+   [gamma.program :as p]
+   [clojure.test :as t])
   (:import [System.IO
             Path File Directory FileInfo DirectoryInfo]))
 
@@ -69,8 +71,8 @@
       :vec3 (throw
               (Exception.
                 (str "can't have vec3 input! name: " name)))
-      :vec4 (str name " (\"" name "\", Vector) = (0, 0, 0, 0)")
-      :float (str name " (\"" name "\", Float) = 0.0")
+      :vec4  (str name " (\"" name "\", Vector) = (0, 0, 0, 0)")
+      :float(str name " (\"" name "\", Float) = 0.0")
       :else (throw
               (Exception.
                 (str
@@ -140,6 +142,15 @@
 ;; ============================================================
 ;; maybe this should be in a different namespace. who can know.
 
+;; (defn op-type-dispatch [& args]
+;;   (let [c (count args)]
+;;     (cond
+;;       (= 0 c) :0
+;;       (= 1 c) :1
+;;       (= 2 c) [:2 (map :type args)]
+;;       (< 2 c) :3+
+;;       )))
+
 (defn g<
   ([_] true)
   ([a b] (g/< a b))
@@ -156,11 +167,125 @@
    (reduce g/+
      (cons arg args))))
 
+;; this is pointless without more info about the shadertoy spec. does
+;; the type of the larger vector win for the type of the return, for
+;; example? that would make sense because it would preserve
+;; commutativity for the commutative ops without breaking it for the
+;; non-commutative ops, and is roughly consistent with adding scalars
+;; to vectors to get a vector
+
+(defn vector-type? [v]
+  (#{:vec2 :vec3 :vec4}
+   (:type (g/ensure-term v))))
+
+(defn scalar-type? [v]
+  (#{:float :int}
+   (:type (g/ensure-term v))))
+
+(defn vector-length [v]
+  (case (:type (g/ensure-term v))
+    :vec2 2
+    :vec3 3
+    :vec4 4))
+
+(def constructor-log
+  (atom []))
+
+(defn constructor [node]
+  (swap! constructor-log conj node)
+  (case (:type (g/ensure-term node))
+    :vec2 g/vec2
+    :vec3 g/vec3
+    :vec4 g/vec4))
+
+;; until gamma.api/aget successfully coerces its index to int
+(defn aget [x i]
+  (g/aget x (g/int i)))
+
+(defn dest [v]
+  (let [n (vector-length v)]
+    (map #(aget v %)
+      (range n))))
+
+(defn- vector-map-op [op a b]
+  (let [[[short-len short]
+         [long-len long]] (sort-by first
+                            (map (juxt vector-length identity) [a b])),
+         smap {short (apply (constructor long)
+                       (take long-len
+                         (concat
+                           (take short-len (dest long))
+                           (repeat 0))))}]
+    (if (= short-len long-len)
+      (op a b)
+      (apply op
+        (replace smap [a b])))))
+
+(defn- vector-scalar-op [op a b]
+  (let [[a b] (map g/ensure-term [a b])
+        [[v] [s]] ((juxt filter remove) vector-type? [a b])
+        smap {s (apply (constructor v) s)}]
+    (apply op
+      (replace smap [a b]))))
+
+(defn- vector-scalar-buddy [op a b]
+  (let [a (g/ensure-term a)
+        b (g/ensure-term b)
+        tinf (set (map #(cond
+                          (vector-type? %) :vector
+                          (scalar-type? %) :scalar)
+                    [a b]))]
+    (case tinf
+      #{:vector} (vector-map-op op a b)
+      #{:vector :scalar} (vector-scalar-op op a b)
+      #{:scalar} (op a b))))
+
+;; (require '[clojure.test :as t])
+
+(defn- run-tests [& args]
+  (binding [t/*test-out* *out*]
+    (apply t/run-tests args)))
+
+(defn- remove-id [x]
+  (clojure.walk/prewalk
+    (fn [y]
+      (if (map? y)
+        (dissoc y :id)
+        y))
+    x))
+
+;; (defn- equiv? [x y]
+;;   (= (remove-id x) (remove-id y)))
+
+;; (t/deftest test-vector-scalar-buddy
+;;   (let [f #(vector-scalar-buddy g/- %1 %2)]
+;;     (t/is
+;;       (equiv? (f 1 2) (g/- 1 2)))
+;;     (t/is
+;;       (equiv?
+;;         (f
+;;           (g/vec3 1 2 3)
+;;           (g/vec4 5 6 7 8))
+;;         (g/-
+;;           (g/vec4 1 2 3 0)
+;;           (g/vec4 5 6 7 8))))
+;;     (t/is
+;;       (equiv?
+;;         (f
+;;           (g/vec4 5 6 7 8)
+;;           (g/vec3 1 2 3))
+;;         (g/-
+;;           (g/vec4 1 2 3 0)
+;;           (g/vec4 5 6 7 8))))))
+
 (defn g-
-  ([arg] (g/* arg -1))
-  ([arg & args]
+  ([a] (g/* a -1))
+  ([a b]
+   ;;(g/- a b)
+   (vector-scalar-buddy g/- a b))
+  ([a b & cs]
    (reduce g/-
-     (cons arg args))))
+     (list* a b cs))))
 
 (defn g*
   ([] 1)
@@ -176,14 +301,16 @@
      (cons arg args))))
 
 (defn gx [v]
-  (g/aget v 0))
+  (aget v 0))
 
 (defn gy [v]
-  (g/aget v 1))
+  (aget v 1))
 
 (defn gz [v]
-  (g/aget v 2))
+  (aget v 2))
 
 (defn gw [v]
-  (g/aget v 3))
+  (aget v 3))
+
+
 
