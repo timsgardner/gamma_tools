c1d8fb0a9f7d88e72a1440b847d19304ef1e92e4
timsgardner <timsgardner@gmail.com>
2016-08-16 00:10:04 -0400

    adding project notes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.timsg/gamma-tools "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tools for using my port of Kovas Boguta's Gamma library"
+  ;; :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;; [org.clojure/clojure "1.8.0"]
